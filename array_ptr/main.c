be38ee1edba91c24762f62fa06985ec112a48223
cf16 <cf16rocket@gmail.com>
2015-05-13 11:10:29 +0200

    init pointer to element with pointer to array

---
+++ array_ptr/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a[3] = {10,20,30};
+    int *b = a;       // ok
+    int *b = (int*)a; // ok
+    int *b = &a;      // error
+    printf( "%d\n",*b++);
+    return 0;
+}
