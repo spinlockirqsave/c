70f73c8a841adf028ede76b4268772baa641b445
spinlockirqsave <mygemailpublic@gmail.com>
2015-11-14 18:18:48 +0000

    negative integers, error for -128

--- complement/main.c
+++ complement/main.c
@@ -8,6 +8,7 @@ main()
     uint8_t u;
 
     // positive integers:
+    printf("\nPositive integers\n");
     i = 1;
     // 2's complement conversion
     // 1. complement in unsigned arithmetic:
@@ -15,6 +16,48 @@ main()
     i_2_c = (int8_t) u;
     printf( "integer [%d]\n"
 		"complement in unsigned arithmetic [%u]\n"
-     		"casted to int again [%d]\a\n", i, u, i_2_c);
+     		"casted to int again [%d]\n", i, u, i_2_c);
+
+    i = 127;
+    // 2's complement conversion
+    // 1. complement in unsigned arithmetic:
+    u = ((1 << 8) - i) & 0xff;
+    i_2_c = (int8_t) u;
+    printf( "integer [%d]\n"
+		"complement in unsigned arithmetic [%u]\n"
+     		"casted to int again [%d]\n", i, u, i_2_c);
+
+    // negative integers:
+    printf("\nNegative integers\n");
+    i = -1;
+    // 2's complement conversion
+    // 1. complement in unsigned arithmetic:
+    u = ((1 << 8) - i) & 0xff; 	// MSB in u is 0 so u treated as unsigned
+				// or signed value is the same
+    i_2_c = (int8_t) u;
+    printf( "integer [%d]\n"
+		"complement in unsigned arithmetic [%u]\n"
+     		"casted to int again [%d]\n", i, u, i_2_c);
+
+    // this will be error, because highiest positive value positive
+    // that can be in expressed in signed 8bit is 127
+    i = -128;
+    // 2's complement conversion
+    // 1. complement in unsigned arithmetic:
+    u = ((1 << 8) - i) & 0xff; 	// MSB in u is 1, will OVERFLOW ! 
+    i_2_c = (int8_t) u;
+    printf( "integer [%d]\n"
+		"complement in unsigned arithmetic [%u]\n"
+     		"casted to int again [%d], ERROR !\n", i, u, i_2_c);
+
+    i = -127;
+    // 2's complement conversion
+    // 1. complement in unsigned arithmetic:
+    u = ((1 << 8) - i) & 0xff; 	// MSB in u is 0 so u treated as unsigned
+				// or signed value is the same
+    i_2_c = (int8_t) u;
+    printf( "integer [%d]\n"
+		"complement in unsigned arithmetic [%u]\n"
+     		"casted to int again [%d]\n", i, u, i_2_c);
     return 0;
 }
