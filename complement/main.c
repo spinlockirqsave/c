faa79eeed25e1ae28a73e5973b1536a38e80bf35
spinlockirqsave <mygemailpublic@gmail.com>
2015-11-15 19:11:37 +0000

    -5

--- complement/main.c
+++ complement/main.c
@@ -39,18 +39,28 @@ main()
 		"complement in unsigned arithmetic [%u]\n"
      		"casted to int again [%d]\n", i, u, i_2_c);
 
-    // this will be error, because highiest positive value positive
+    // this will be an error, because highiest positive value
     // that can be in expressed in signed 8bit is 127
     i = -128;
     // 2's complement conversion
     // 1. complement in unsigned arithmetic:
-    u = ((1 << 8) - i) & 0xff; 	// MSB in u is 1, will OVERFLOW ! 
+    u = ((1 << 8) - i) & 0xff; 	// MSB in u is 1, OVERFLOW ! 
     i_2_c = (int8_t) u;
     printf( "integer [%d]\n"
 		"complement in unsigned arithmetic [%u]\n"
      		"casted to int again [%d], ERROR !\n", i, u, i_2_c);
 
     i = -127;
+    // 2's complement conversion
+    // 1. complement in unsigned arithmetic:
+    u = ((1 << 8) - i) & 0xff; 	// MSB in u is 0 so u treated as unsigned
+				// or signed value is the same
+    i_2_c = (int8_t) u;
+    printf( "integer [%d]\n"
+		"complement in unsigned arithmetic [%u]\n"
+     		"casted to int again [%d]\n", i, u, i_2_c);
+
+    i = -5;
     // 2's complement conversion
     // 1. complement in unsigned arithmetic:
     u = ((1 << 8) - i) & 0xff; 	// MSB in u is 0 so u treated as unsigned
