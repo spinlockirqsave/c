8a904a5baf27cfbff23df6aaa71bcd64e13f961a
spinlockirqsave <mygemailpublic@gmail.com>
2015-11-15 19:13:53 +0000

    abs overflow on max negative value -2^N representable by Nbit integer

---
+++ abs/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <complex.h>
+#include <stdlib.h>
+
+int
+main()
+{
+	int8_t i;
+	i = -127;
+    	printf( "i: [%d]\n", i);
+	i = abs(i);
+    	printf( "abs(i): [%d]\n", i);
+
+	i = -128;
+    	printf( "i: [%d]\n", i);
+	i = abs(i);
+    	printf( "abs(i): [%d]\n", i);
+    return 0;
+}
