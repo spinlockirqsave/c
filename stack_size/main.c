38f6c5b3b06724564c2316ac1605f3eca8a14b50
Piotr Gregor <piotrek.gregor@gmail.com>
2016-04-08 22:18:03 +0100

    stack size

--- stack_size/main.c
+++ stack_size/main.c
@@ -1,13 +1,16 @@
+#include <stdio.h>
+
 void func()
 {
-    static int i=0;
-    int arr[1024]={0};
-    printf("%ld KB pushed on stack!\n",++i*sizeof(int));
+    static int i = 0;
+    int arr[1024]= {0};
+    printf("%zu KB pushed on stack!\n", ++i * sizeof(int));
     func();
 }
 
 int main()
 {
     func();
+    printf("sizeof(short) [%zu]\n", sizeof(short));
     return 0;
 }
