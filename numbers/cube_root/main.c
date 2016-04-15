d204b912c6abcd93cc0e4a0cb83699a22fbc1ed4
Piotr Gregor <piotrek.gregor@gmail.com>
2016-04-15 15:07:09 +0100

    cube root

---
+++ numbers/cube_root/main.c
@@ -0,0 +1,16 @@
+/*
+ * @file    main.c
+ * @author  Piotr Gregor <piotrek.gregor gmail com>
+ * @brief   
+*/
+
+
+#include <stdio.h>
+
+
+int
+main(void)
+{
+    printf( "Hello world\a\n");
+    return 0;
+}
